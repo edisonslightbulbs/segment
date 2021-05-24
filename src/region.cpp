474431bab899e36cbbed3e6dc837f2a7ce419c5d
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-05-24 15:18:55 +0200

    migrating query boundary implementation

--- src/region.cpp
+++ src/region.cpp
@@ -7,43 +7,18 @@
 
 std::vector<Point> region::segment(std::vector<Point>& points)
 {
-    /** remove outliers */
-    std::vector<Point> denoisedPcl = outliers::filter(points);
+    // remove outliers
+    std::vector<Point> denoised = outliers::filter(points);
 
-    /** compute svd */
-    std::pair<Eigen::JacobiSVD<Eigen::MatrixXf>, Eigen::MatrixXf> USV;
-    USV = svd::compute(denoisedPcl);
+    // compute svd
+    int flag = Eigen::ComputeThinU | Eigen::ComputeThinV;
+    SVD usv(denoised, flag);
 
-    /** extract coarse segment */
-    std::vector<Point> coarseSeg = proposal::grow(USV, denoisedPcl);
+    // course segment
+    std::vector<Point> coarse = proposal::grow(usv, denoised);
 
-    /** extract final segment */
-    std::vector<Point> finalSeg = outliers::filter(coarseSeg);
+    // final segment
+    std::vector<Point> final = outliers::filter(coarse);
 
-    /** return interaction context */
-    return finalSeg;
-}
-
-std::pair<Point, Point> region::queryBoundary(std::vector<Point>& points)
-{
-    std::vector<int16_t> X(points.size());
-    std::vector<int16_t> Y(points.size());
-    std::vector<int16_t> Z(points.size());
-    for (auto& point : points) {
-        X.push_back(point.m_xyz[0]);
-        Y.push_back(point.m_xyz[1]);
-        Z.push_back(point.m_xyz[2]);
-    }
-
-    /** find boundary given a set of points */
-    int16_t xMax = (int16_t)*std::max_element(X.begin(), X.end());
-    int16_t xMin = (int16_t)*std::min_element(X.begin(), X.end());
-    int16_t yMax = (int16_t)*std::max_element(Y.begin(), Y.end());
-    int16_t yMin = (int16_t)*std::min_element(Y.begin(), Y.end());
-    int16_t zMax = (int16_t)*std::max_element(Z.begin(), Z.end());
-    int16_t zMin = (int16_t)*std::min_element(Z.begin(), Z.end());
-
-    Point min(xMin, yMin, zMin);
-    Point max(xMax, yMax, zMax);
-    return { min, max };
+    return final;
 }
