78b3bd412843c3d62770b23de776dff6eab5ae04
everett <mondliwethu.everett.mthunzi@uni-weimar.de>
2021-05-24 15:18:36 +0200

    migrating query boundary implementation

--- include/region.h
+++ include/region.h
@@ -1,9 +1,8 @@
 #ifndef REGION_H
 #define REGION_H
 
-#include <vector>
-
 #include "point.h"
+#include <vector>
 
 namespace region {
 
@@ -19,16 +18,5 @@ namespace region {
  */
 std::vector<Point> segment(std::vector<Point>& points);
 
-/**
- * queryBoundary
- *   Queries the min max point boundaries of segmented context.
- *
- * @param points
- *   Segmented interaction context.
- *
- * @retval
- *    { Point_min, Point_max }
- */
-std::pair<Point, Point> queryBoundary(std::vector<Point>& points);
 }
 #endif /* REGION_H */
